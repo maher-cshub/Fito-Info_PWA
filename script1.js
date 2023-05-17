7506266c43ca7a42bbbdb81145871de957593a31
maher-cshub <maherarab.1299@gmail.com>
2023-05-17 02:17:32 +0100

    final user

--- script1.js
+++ script1.js
@@ -1,23 +1,45 @@
+
+import firebase_app from "./util.js"
+import {getDatabase , ref ,onValue,onChildAdded,onChildRemoved} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 let user_search_input = document.getElementById("user-search-input");
-let all_items = document.getElementById("results-area");
-let info_btns = document.querySelectorAll("#item-info-btn");
+user_search_input.oninput = search_items;
+
+const database = getDatabase(firebase_app);
+
+const results_area = document.getElementById("results-area");
+
+document.addEventListener("DOMContentLoaded",refreshData);
+
+
+let info_btns = null;
 
 
 //events
 
-user_search_input.oninput = search_items;
+function initVars(){
+    info_btns = document.querySelectorAll("#item-info-btn");
+    info_btns.forEach(button =>{
+        button.addEventListener("click",getInfo);
+    })
+    return
+}
 
-info_btns.forEach(button =>{
-    button.addEventListener("click",getInfo);
-})
+
+function getInfo(e){
+    //get item id 
+    const chosen_item = e.target.offsetParent.parentElement;
+    let item_id = chosen_item.getAttribute("item_id")
+    localStorage.setItem("selected_item_to_preview",item_id)
+    window.location.href = "details.html"
+}
+
 
 
 function search_items(e){
+    let all_items = document.getElementById("results-area");
     let input = String(e.srcElement.value);
     let items = Array.from(all_items.children);
-    console.log(items)
     items.forEach(element => {
-        console.log(element.children[1].childNodes[1].innerText)
         if(String(element.children[1].childNodes[1].innerText).trim().toLowerCase().startsWith(input.trim().toLowerCase())){
             element.style.removeProperty("display");
         }
@@ -29,15 +51,96 @@ function search_items(e){
 
 }
 
-function getInfo(e){
-    //get item id 
-    const chosen_item = e.target.offsetParent.parentElement
-    item_object = {}
-    item_object["item_id"] = chosen_item.getAttribute("item_id")
-    item_object["item_image"] = chosen_item.querySelector("#item-image > img").getAttribute("src")
-    item_object["item_name"] = chosen_item.querySelector("#item-title").innerText
-        /*image and name
-    */
-    localStorage.setItem("selected_item",JSON.stringify(item_object))
-    window.location.href = "details.html"
+function saveElement(element){
+    const item = document.createElement("div")
+    item.setAttribute("id","item");
+    item.setAttribute("item_id",element[0]);
+    item.innerHTML = `
+        <div id="item-image">
+            <img src=${element[1]["image"]} alt=${element[1]["name"]}>
+        </div>
+        <div id="item-details">
+            <h1 id="item-title">${element[1]["name"]}</h1>
+            <button class="info-btn-cls" id="item-info-btn">GET INFO</button>
+
+        </div>
+    `
+    
+    results_area.appendChild(item);
+    return
 }
+
+async function getAllItems(){
+    try {  
+        const items_ref = ref(database,"items");
+        await onValue(items_ref,function(snapshot){
+            if (snapshot.exists()){
+                let items = Object.entries(snapshot.val())
+                localStorage.removeItem("items");
+                localStorage.setItem("items",JSON.stringify(items))
+            }
+            else{
+                localStorage.removeItem("items");
+                localStorage.setItem("items",JSON.stringify([]))
+            }
+        });
+    } catch (error) {
+        alert("please check your internet connection")
+    }
+}
+
+
+async function refreshData(){
+    
+    results_area.innerHTML = "";
+    
+    await getAllItems(); 
+
+    let items = JSON.parse(localStorage.getItem("items"));
+
+    if (items.length > 0){
+        items.forEach(element => {
+            saveElement(element);
+        });
+    }
+    initVars()
+    return
+}
+
+
+onChildAdded(ref(database,"items"),(snapshot)=>{
+    
+    let items =  JSON.parse(localStorage.getItem("items"));
+    if (items == null || items == undefined)
+    {
+        refreshData()
+        return
+    }
+
+    //check if child exist in client
+    const exist = items.some((element)=>{
+        return element[1]["name"] == snapshot.val()["name"]
+    })
+    if (exist == false){
+        refreshData()
+        return
+    }
+})
+
+
+//--------------------item removed (done)
+onChildRemoved(ref(database,"items"),(snapshot)=>{
+    const deleted_item = document.querySelector(`[item_id="${snapshot.key}"]`);
+    deleted_item.remove();
+    let items = JSON.parse(localStorage.getItem("items"));
+    items = items.filter(item => !(item[0] == snapshot.key));
+    localStorage.setItem("items",items);
+})
+
+
+setInterval(()=>{
+    localStorage.removeItem("items");
+    refreshData()
+},60000)
+
+refreshData()
--- script2.js
+++ script2.js
@@ -1,27 +1,79 @@
-var coll = document.getElementsByClassName("collapsible");
-let item_image = document.querySelector("#item-image");
-let current_page = document.getElementsByTagName("html");
+import firebase_app from "./util.js";
+import {getDatabase,ref,get} from  "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      // content.scrollHeight + "px"
-      content.style.maxHeight = "70vh";
-    } 
-    content.classList.toggle("active-content");
+let item_image = document.querySelector("#item-image");
+
+
+function updateDOM(){
+  var coll = document.getElementsByClassName("collapsible");
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        // content.scrollHeight + "px"
+        content.style.maxHeight = "70vh";
+      } 
+      content.classList.toggle("active-content");
+    });
+  }
+}
+
+function setContent(items){
+  let infos = Object.entries(items)
+  const contentarea = document.getElementById("content-area");
+  infos.forEach(info => {
+    const div1 = document.createElement("div");
+    const div2 = document.createElement("div");
+    div1.setAttribute("class","collapsible");
+    div2.setAttribute("class","content");
+    div1.innerHTML = `<span>${info[0]}</span>`
+    div2.innerHTML = `<iframe srcdoc='${info[1]}' frameborder="0"></iframe>`
+    contentarea.appendChild(div1);
+    contentarea.appendChild(div2);
   });
 }
 
+function LoadPage(){
+  //current_page[0].style.opacity = 0;
+  const database = getDatabase(firebase_app);
+  let target_id = localStorage.getItem("selected_item_to_preview");
+  const target_ref = ref(database,`items/${target_id}`);
+  get(target_ref)
+  .then((snapshot)=>{
+    let item = snapshot.val()
+    item_image.querySelector("img").setAttribute("src",item["image"])
+    item_image.querySelector("img").setAttribute("onerror","this.src='/blank.jpg'")
+    item_image.querySelector("span").innerText = item["name"];
+    if (item["infos"] != null || item["infos"] != undefined)
+    {
+      setContent(item["infos"])
+    }
+    updateDOM()
+    
 
 
-  document.addEventListener("DOMContentLoaded", e =>{
-    //current_page[0].style.visibility = "hidden";
-    target_object = JSON.parse(localStorage.getItem("selected_item"));
-    item_image.querySelector("img").setAttribute("src",target_object["item_image"])
-    item_image.querySelector("span").innerText = target_object["item_name"];
-   // current_page[0].style.visibility = "visibile";
+    //current_page[0].style.opacity = 1;
+    
+  })
+   
+
+
+  .catch((error) => {
+  })
+  
+  
+}
+
+
+document.addEventListener("DOMContentLoaded", e =>{
+  LoadPage()
+  const loader = document.querySelector(".loader-active");
+  loader.classList.add("loader-hidden")
+  loader.addEventListener("transistioned",()=>{
+    loader.remove()
+  })
 })
+
